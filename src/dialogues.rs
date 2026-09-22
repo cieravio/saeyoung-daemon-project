fa6675b9675457060871f81d9d7b89bc1ba92f93
cieravio <cieraviolina@gmail.com>
2026-09-22 14:43:02 +0700

    add randomized dialogue feature

--- src/dialogues.rs
+++ src/dialogues.rs
@@ -11,4 +11,8 @@ impl Dialogues {
         let data = fs::read_to_string(path).expect("failed to read dialogues.json");
         serde_json::from_str(&data).expect("invalid JSON format")
     }
+
+    pub fn random_from(&self, category: &str) -> Option<&String> {
+        self.0.get(category)?.choose(&mut rand::thread_rng())
+    }
 }
