5b537c8a7d876a14e561d9db98bb7866f1bfb940
cieravio <cieraviolina@gmail.com>
2026-09-22 21:52:36 +0700

    update delay to 15 mins

--- src/connection.rs
+++ src/connection.rs
@@ -1,14 +1,16 @@
 use tokio::net::TcpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::time::{interval, Duration};
+use tokio::time::{interval_at, Instant, Duration};
 use crate::dialogues::Dialogues;
 
 pub async fn handle_connection(mut stream: TcpStream, dialogues: Dialogues) {
+    let _ = stream.write_all(b"Connected to Agent 707.\n").await;
     if let Some(msg) = dialogues.random_from("connect_success") {
         let _ = stream.write_all(format!("{msg}\n").as_bytes()).await;
     }
 
-    let mut ticker = interval(Duration::from_secs(300));
+    let delay = Duration::from_secs(900);
+    let mut ticker = interval_at(Instant::now() + delay, Duration::from_secs(300));
     let mut buf = [0u8; 1024];
 
     loop {
